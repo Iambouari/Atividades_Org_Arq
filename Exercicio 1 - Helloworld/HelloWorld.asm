e0eae8a9b04d19bd9238c4997ac576ab915b4cf3
Yazid Bouari <yazidbouari@usp.br>
2023-11-21 22:33:52 -0300

    Add files via upload

---
+++ Exercicio 1 - Helloworld/HelloWorld.asm
@@ -0,0 +1,28 @@
+
+;==============================================
+; 				Hello world
+;==============================================
+
+jmp main
+
+HelloWorld: string "Hello, World!"
+
+main:
+    call ApagaTela
+
+    loadn R1, #10
+    load R2, HelloWorld
+    call ImprimeString
+
+    rts
+
+ImprimeString:
+    outchar R2, R1
+    inc R1
+    load R3, [R2]
+    cmp R3, #0   
+    jeq ImprimeString_End
+    jmp ImprimeString
+
+ImprimeString_End:
+    rts
